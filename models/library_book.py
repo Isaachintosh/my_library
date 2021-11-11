fb248df913fbcdcc2a02aa386d6f8bb063aa0396
isaachintosh <isaac.tuining@gmail.com>
2021-11-11 13:53:19 -0300

    constraints & compute fields has been add - WIP

--- models/library_book.py
+++ models/library_book.py
@@ -1,6 +1,7 @@
-from odoo import fields, models
+from odoo import fields, models, api
 from odoo.addons import decimal_precision as dp
-
+from odoo.exceptions import ValidationError
+from datetime import timedelta
 
 class LibraryBook(models.Model):
     _name = 'library.book'
@@ -49,13 +50,61 @@ class LibraryBook(models.Model):
         domain=[],
         )
     category_id = fields.Many2one('library.book.category')
+    age_days = fields.Float(
+        string='Days Since Release',
+        compute='_compute_age',
+        inverse='_inverse_age',
+        search='_search_age',
+        store=False,
+        compute_sudo=False,
+        )
 
-def name_get(self):
-    result = []
-    for record in self:
-        rec_name = "%s (%s)" % (record.name, record.date_release)
-        result.append((record.id, rec_name))
-        return result
+    def name_get(self):
+        result = []
+        for record in self:
+            rec_name = "%s (%s)" % (record.name, record.date_release)
+            result.append((record.id, rec_name))
+            return result
+
+    @api.depends('date_release')
+    def _compute_age(self):
+        today = fields.Date.today()
+        for book in self.filtered('date_release'):
+            delta = today - book.date_release
+            book.age_days = delta.days
+
+    def _inverse_age(self):
+        today = fields.Date.today()
+        for book in self.filtered('date_release'):
+            d = today - timedelta(days=book.age_days)
+            book.date_release = d
+    
+    def _search_age(self, operator, value):
+        today = fields.Date.today()
+        value_days = timedelta(days=value)
+        value_date = today - value_days
+        operator_map = {
+            '>': '<',
+            '>=': '<=',
+            '<': '>',
+            '<=': '>=',
+        }
+
+    _sql_constraints = [
+        ('name_uniq', 'UNIQUE (name)', 'Book title must be unique.'),
+        # ('positive_page', 'CHECK(pages>0)', 'No of pages must be positive')
+    ]
+
+    @api.constrains('date_release')
+    def _check_release_date(self):
+        for record in self:
+            if record.date_release and record.date_release > fields.Date.today():
+                raise models.ValidationError('Release date must be in the past.')
+    
+    # def _check_num_pages(self):
+    #     for record in self:
+    #         if record.pages and record.pages <= 0:
+    #             raise models,ValidationError('Number of pages must be positive.')
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
