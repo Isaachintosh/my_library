a5f3c7485dd358b8ff5543fdf240e87462adfc09
Isaachintosh <isaac.tuning@gmail.com>
2021-11-23 21:03:45 -0300

    WIP methods of return books are under development

--- wizard/book_rent_wizard.py
+++ wizard/book_rent_wizard.py
@@ -22,41 +22,4 @@ class LibraryRentWizard(models.TransientModel):
             action['view_mode'] = 'tree,form'
         return action
 
-    def books_returns(self):
-        loan = self.env['library.book.rent']
-        for rec in self:
-            loans = loan.search(
-                [('state', '=', 'ongoing'),
-                ('book_id', '=', rec.book_ids.id),
-                ('borrower_id', '=', rec.borrower_id.id)]
-            )
-            for loan in loans:
-                loan.book_return
-
-    @api.onchange('borrower_id')
-    def onchange_member(self):
-        rentModel = self.env['library.book.rent']
-        books_on_rent = rentModel.search(
-            [('state', '=', 'ongoing'),
-            ('borrower_id', '=', self.borrower_id.id)]
-        )
-        self.book_ids = books_on_rent.mapped('book_id')
-        result = {
-            'domain': {
-                'book_ids': [
-                    ('id', 'in', self.book_ids.ids)
-                ]
-            }
-        }
-        late_books = [
-            ('id', 'in', books_on_rent.ids),
-            ('expected_return_date', '<', fields.Date.today())
-        ]
-        if late_books:
-            message = ('Warn the member that the following books are late:\n')
-            titles = late_books.mapped('book_id.name')
-            result['warning'] = {
-                'title': 'Late books',
-                'message': message + '\n'.join(titles)
-            }
-            return result
+    
